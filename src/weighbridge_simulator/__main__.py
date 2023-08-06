e47872b71e07f2b1a27301302db9b4deaedd6283
Garrett HE <garrett.he@hotmail.com>
2023-08-06 10:26:48 +0800

    Add feature of serial communicating from data files

--- src/weighbridge_simulator/__main__.py
+++ src/weighbridge_simulator/__main__.py
@@ -1,11 +1,24 @@
+import time
+from typing import TextIO
+
 import click
+from serial import Serial
 
 
 @click.command()
+@click.option('-p', '--port', help='Name of serial port.', type=str, required=True)
+@click.option('-d', '--data-file', help='Path of data file.', type=click.File('r', encoding='ascii'))
 @click.version_option(message='%(version)s')
-def cli():
+def cli(port: str, data_file: TextIO):
     """A command line tool continuously send data to a serial port to simulate weighbridge communicating."""
+
+    ser = Serial(port)
+    for line in data_file.readlines():
+        ser.write(line.strip()[::-1].encode('ascii') + b'=')
+        time.sleep(0.2)
+
+    ser.close()
 
 
 if __name__ == '__main__':  # pragma: no cover
-    cli()
+    cli()  # pylint: disable=no-value-for-parameter
