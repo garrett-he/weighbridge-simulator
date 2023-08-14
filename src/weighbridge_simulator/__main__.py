62693b6bf24fef6d0c619c43718e4990123b979d
Garrett HE <garrett.he@hotmail.com>
2023-08-14 10:28:17 +0800

    Add option to specify loops of sending data set

--- src/weighbridge_simulator/__main__.py
+++ src/weighbridge_simulator/__main__.py
@@ -6,16 +6,23 @@ from serial import Serial
 
 
 @click.command()
-@click.option('-p', '--port', help='Name of serial port.', type=str, required=True)
-@click.option('-d', '--data-file', help='Path of data file.', type=click.File('r', encoding='ascii'))
+@click.option('-p', '--port', help='Name of serial port.', metavar='NAME', type=str, required=True)
+@click.option('-d', '--data-file', help='Path of data file.', metavar='PATH', type=click.File('r', encoding='ascii'),
+              required=True)
+@click.option('-l', '--loops', help='Loops of sending data set, zero means endless.', metavar='N', type=int, default=1)
 @click.version_option(message='%(version)s')
-def cli(port: str, data_file: TextIO):
+def cli(port: str, data_file: TextIO, loops: int):
     """A command line tool continuously send data to a serial port to simulate weighbridge communicating."""
 
     ser = Serial(port)
-    for line in data_file.readlines():
-        ser.write(line.strip()[::-1].encode('ascii') + b'=')
-        time.sleep(0.2)
+    i = 0
+
+    while loops == 0 or i < loops:
+        for line in data_file.readlines():
+            ser.write(line.strip()[::-1].encode('ascii') + b'=')
+            time.sleep(0.2)
+
+        i += 1
 
     ser.close()
 
