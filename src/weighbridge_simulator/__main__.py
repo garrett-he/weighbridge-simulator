ff957278205af164f0371930dffe07940b81f07f
Garrett HE <garrett.he@hotmail.com>
2023-09-25 13:31:33 +0800

    Fix issue of show version in pyinstaller

--- src/weighbridge_simulator/__main__.py
+++ src/weighbridge_simulator/__main__.py
@@ -6,15 +6,25 @@ from typing import TextIO
 
 import click
 from serial import Serial
+from weighbridge_simulator import __version__
+
+
+def print_version(ctx: click.Context, _, value: str):
+    if not value or ctx.resilient_parsing:
+        return
+
+    click.echo(__version__)
+    ctx.exit()
 
 
 @click.command()
+@click.option('--version', help='Show version information.', is_flag=True, callback=print_version, expose_value=False,
+              is_eager=True)
 @click.option('-p', '--port', help='Name of serial port.', metavar='NAME', type=str, default=None)
 @click.option('-d', '--data-file', help='Path of data file.', metavar='PATH', type=click.File('r', encoding='ascii'),
               required=True)
 @click.option('-l', '--loops', help='Loops of sending data set, zero means endless.', metavar='N', type=int, default=1)
 @click.option('-i', '--interval', help='Interval of each data.', metavar='SECS', type=float, default=0.2)
-@click.version_option(message='%(version)s')
 def cli(port: str, data_file: TextIO, loops: int, interval: float):
     """A command line tool continuously send data to a serial port to simulate weighbridge communicating."""
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,11 @@
-import importlib.metadata
-
 from click.testing import CliRunner
+
+from weighbridge_simulator import __version__
 from weighbridge_simulator.__main__ import cli
 
 
 def test_cli_version():
-
     result = CliRunner().invoke(cli, ['--version'])
 
     assert not result.exception
-    assert result.output.strip() == importlib.metadata.version('weighbridge_simulator')
+    assert result.output.strip() == __version__
