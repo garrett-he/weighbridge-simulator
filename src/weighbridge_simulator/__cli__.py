844749ae0bf93381355f1173fa968dea92d8bd45
Garrett HE <garrett.he@outlook.com>
2024-01-24 10:29:47 +0800

    Add feature to create new PTY internally

--- src/weighbridge_simulator/__cli__.py
+++ src/weighbridge_simulator/__cli__.py
@@ -1,4 +1,7 @@
+import os
+import pty
 import time
+import tty
 from typing import TextIO
 
 import click
@@ -17,24 +20,34 @@ def print_version(ctx: click.Context, _, value: str):
 
 @click.command()
 @click.option('--version', help='Show version information.', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
-@click.option('-p', '--port', help='Name of serial port.', metavar='NAME', type=str, required=True)
+@click.option('-p', '--port', help='Name of serial port.', metavar='NAME', type=str, default=None)
 @click.option('-d', '--data-file', help='Path of data file.', metavar='PATH', type=click.File('r', encoding='ascii'), required=True)
 @click.option('-l', '--loops', help='Loops of sending data set, zero means endless.', metavar='N', type=int, default=1)
-@click.option('-i', '--interval', help='Interval of each data.', type=float, default=0.2)
+@click.option('-i', '--interval', help='Interval of each data.', metavar='SECS', type=float, default=0.2)
 def main(port: str, data_file: TextIO, loops: int, interval: float):
     """A command line tool continuously send data to a serial port to simulate weighbridge communicating."""
 
-    ser = Serial(port)
+    if port is None:
+        master, slave = pty.openpty()
+        tty.setraw(master)
+        os.set_blocking(master, False)
+        out = open(master, 'r+b', buffering=0)  # pylint: disable=consider-using-with
+        port_name = os.ttyname(slave)
+
+        click.echo(f'Created PTY: {port_name}')
+    else:
+        out = Serial(port)
+        port_name = port
 
     i = 0
 
     lines = data_file.readlines()
     while loops == 0 or i < loops:
-        with click.progressbar(lines, label=f'[{i}/{loops}] Sending data set to port: {port}') as items:
+        with click.progressbar(lines, label=f'[{i}/{loops}] Sending data set to port: {port_name}') as items:
             for item in items:
-                ser.write(item.strip()[::-1].encode('ascii') + b'=')
+                out.write(item.strip()[::-1].encode('ascii') + b'=')
                 time.sleep(interval)
 
         i += 1
 
-    ser.close()
+    out.close()
